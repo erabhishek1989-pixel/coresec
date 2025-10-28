867d637a366b55d6868a784e41a1a10e10e81453
erabhishek1989-pixel <er.abhishek1989@gmail.com>
2025-10-28 00:10:04 +0000

    Add new variables for Azure configuration

--- modules/sentinel_workspace/variables.tf
+++ modules/sentinel_workspace/variables.tf
@@ -22,3 +22,35 @@ variable "sku" {
 variable "resource_group_name" {
   type = string
 }
+
+# NEW: Add these variables
+variable "tenant_id" {
+  description = "Azure AD Tenant ID"
+  type        = string
+}
+
+variable "workspace_shared_key_secret_uri" {
+  description = "Key Vault secret URI for workspace shared key"
+  type        = string
+  default     = null
+}
+
+variable "application_insights_key" {
+  description = "Application Insights key"
+  type        = string
+  default     = null
+}
+
+variable "connectors" {
+  description = "Map of data connectors"
+  type = map(object({
+    enabled                  = bool
+    connector_type           = string
+    key_vault_name           = optional(string)
+    key_vault_resource_group = optional(string)
+    endpoint                 = optional(string)
+    secrets                  = optional(map(string))
+    config                   = optional(map(any))
+  }))
+  default = {}
+}
